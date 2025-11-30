2c62430369a4b775ac8960d9956abaf11f6607b1
Pilupus <ili.tara.kori@gmail.com>
2025-11-30 19:57:03 +0900

    Practicing Middleware Configuration

--- 00_beginer_level/02_backend-api_and_micro-service/01_web-app/MyWebApp/Program.cs
+++ 00_beginer_level/02_backend-api_and_micro-service/01_web-app/MyWebApp/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello .NET Developer Community!");
+app.Use(async (context, next) =>
+{
+    await next();
+    Console.WriteLine($"{context.Request.Method} {context.Request.Path} {context.Response.StatusCode}");
+});
+
+app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
+
+app.MapGet("/", () => "Welcome to Contoso!");
+app.MapGet("/about", () => "Contoso was founded in 2000.");
 
 app.Run();
